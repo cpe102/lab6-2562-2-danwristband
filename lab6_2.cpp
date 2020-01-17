dc888d87723db483c287afa2cfba486cbe56f30c
danwristband <59277358+danwristband@users.noreply.github.com>
2020-01-17 16:28:46 +0700

    lab6

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,11 +1,60 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
 //Dialog of the first example is given below.
 
-/*
-Fahsai: Sawadee ka...Can you tell me your name?
-?????: Luke Skywalker
-Fahsai: Wow!!! Luke Skywalker that is a really cool name.
-Fahsai: I think you are Engineering student. What is your student ID?
-Luke Skywalker: 590610999
+
+int main(){
+    string name;
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?"<<endl;
+    cout<<"????:";
+    getline (cin,name);
+    cin.ignore();
+    cout<<"Fahsai: Wow!!! "<<name<<" that is a really cool name."<<endl;
+    cout<<"Fahsai: I think you are Engineering student. What is your student ID?"<<endl;
+
+    string code;
+    cout<<name<<":";
+    cin>>code;
+    cin.ignore();
+
+    int gear=((code[0]-48)*10+(code[1]-48))-12;
+
+    cout<<"Fahsai: I think you may be GEAR "<<gear<<"\n"".  I have a free movie tickets for you. "<<endl;
+    cout<<"Fahsai: Let's go to cinema together!!!"<<endl;
+    cout<<"Fahsai: What movie do you want to watch?"<<endl;
+    string movie;
+     
+    cout<<name<<":";
+    getline (cin,movie);
+    cin.ignore();
+
+    cout<<"Fahsai: So....which day are you free to go with me?"<<endl;
+    cout<<name<<":";
+    string day;
+    getline (cin,day);
+    cin.ignore();
+    string text; 
+    cout<<"Fahsai: "<<day<<"....that is OK!!! I'm looking forward to watch "<<movie<<" with you."<<endl;
+    cout<<name<<":";
+    getline (cin,text);
+    cin.ignore();
+    cout<<"Fahsai: 555+ see you "<<day<<". Bye Bye \(^ ^)/";
+
+
+
+
+    
+
+
+
+
+
+    return 0;
+}
+
+/*Luke Skywalker: 590610999
 Fahsai: I think you may be GEAR 47.  I have a free movie tickets for you. 
 Fahsai: Let's go to cinema together!!! 
 Fahsai: What movie do you want to watch?
@@ -14,5 +63,5 @@ Fahsai: So....which day are you free to go with me?
 Luke Skywalker: Next Monday
 Fahsai: Next Monday....that is OK!!! I'm looking forward to watch Star Wars VII with you.
 Luke Skywalker: May the Force be with you krub
-Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/ 
-*/
+Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)*/ 
+
--- lab6_3.cpp
+++ lab6_3.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+	char y;
+	if(x>=65 && x<=90){
+		if(x==65){
+			return 90;
+		}
+		 else
+		{
+		y=x-1;
+		return y;
+		}
+	
+	}
+	else
+	{ 	return '0';
+	}
+	
+
 }
 
 int main(){
